d7d01c44a24958b29b39eb81ccf9837065fe36b6
Gaurav Tamhan <gaurav.tamhan@gmail.com>
2016-07-22 02:56:03 -0400

    up to date

--- choose_theater.php
+++ choose_theater.php
@@ -2,6 +2,7 @@
    include("config.php");
    session_start();
    $user = $_SESSION['current_user'];
+   $movie = $_SESSION['movie'];
 ?>
 
 <?php   
@@ -18,7 +19,6 @@
       array_push($preferred_theaters, $row["Name"]);
 
    }
-   print_r($preferred_theaters);
 
    // if(isset($_POST['choose'])) {
    //    if(isset($_POST['saved_theater'])){ 
@@ -42,13 +42,14 @@
       <style type = "text/css">
          h1 {
             font-style: italic;
-            font-size: 30px;
+            font-size: 50px;
             text-align: center;
-            margin-top: 40px;
+            margin-top: 60px;
          }
 
          hr {
           width: 70%;
+          margin-bottom: 50px
          }
 
          label {
@@ -66,6 +67,7 @@
             width: 500px;
             border: none;
             margin-top:30px;
+            margin-left: 12%;
          }
 
          select {
@@ -81,12 +83,13 @@
              border: 1px solid #AAA;
              color: #555;
              font-size: inherit;
-             margin: 20px;
+             margin-top: 20px;
+             margin-right: 20px;
              overflow: hidden;
              padding: 5px 10px;
              text-overflow: ellipsis;
              white-space: nowrap;
-             
+             display: inline;
              width: 300px;
 
           }
@@ -110,10 +113,28 @@
             border: 1px solid black;
          }
 
-         input[type=text], input[type=password]{
-             width: 100%;
-             padding: 10px 10px;
-             margin-bottom: 8px;
+         form {
+            margin-top: 50px;
+         }
+
+         label {
+            display: block;
+            width: 70%
+         }
+
+         p {
+            margin-left: 30%;
+            font-style: italic;
+            font-size: 30px;
+            margin-top: 15%;
+            margin-bottom: 15%;
+         }
+
+         input[type=text] {
+             width: 60%;
+             padding: 5px 10px;
+             margin-top: 20px;
+             margin-right: 20px;
              box-sizing: border-box;
              border: 1px solid #ccc;
              -webkit-transition: 0.5s;
@@ -122,14 +143,15 @@
              border-radius: 2px;
              font-family: Georgia;
              font-size: 15px;
+             display: inline;
          }
 
-         input[type=text]:focus, input[type=password]:focus {
+         input[type=text]:focus {
              border: 1px solid black;
          }
 
          input[type=submit] {
-            width: 100%;
+            /*width: 60%;*/
             font-size: 15px;
             background-color: black;
             color: white;
@@ -137,10 +159,12 @@
             border: none;
             font-family: Georgia;
             cursor: hand;
+            border-radius: 3px;
+            display: inline;
          }
 
          .backbutton {
-            margin-top: 0px;
+            margin-top: 30px;
             border-collapse: separate;
         
             height: 50px;
@@ -150,8 +174,9 @@
 
           table {
             margin-top: 10px;
+            margin-bottom: 20px;
             border-collapse: separate;
-            width: 80%;
+            width: 30%;
             border: none;
             border-spacing: 10px;
           }
@@ -193,36 +218,51 @@
    
    <body>
       <h1> Choose a Theater </h1>
+      <hr>
       <div align = "center">
          <div class="container" align = "left">
           
 				
-            <div style = "margin:20px">
+            <div style = "margin:20px auto;">
                
                <form action = "" method = "post"> 
-                  <label> Saved Theater </label><select name="saved_theater">  
+                  <label> Saved Theaters </label><select name="saved_theater">  
                                  <?php
+
                                     $i = 0;
                                     while ($i < $count) {
-                                       echo "<option value="."'".$preferred_theaters[$i]."'>".$preferred_theaters[$i]."</option>";
+                                       $out = strlen($preferred_theaters[$i]) > 25 ? substr($preferred_theaters[$i],0,25)."..." : $preferred_theaters[$i];
+                                       echo "<option value="."'".$preferred_theaters[$i]."'>".$out."</option>";
                                        $i++;
                                     }
                                  ?>                    
                               </select>
                   <input type = "submit" name= "choose" value ="Choose"/><br />
                </form>
-
+               <!-- <br> -->
+               <p> OR </p>
                <form action = "" method = "post"> 
                   <label> City/State/Theater </label>
-                  <input type = "text" name= "user_input" class = "box"/>
+                  <input type = "text" name= "user_input" placeholder="Search..." />
                   <input type = "submit" name= "search" value = " Search "/><br />
                </form>
-					
+               <br>
+               <br>
+
             </div>
 				
          </div>
 			
       </div>
+
+       <hr>
+      <table align="center" class="backbutton">
+         <tr>
+            <td>
+               <a href="movie.php?movie=<?php echo $movie ?>"> Back </a>
+            </td>
+         </tr>
+      </table>
 
    </body>
 </html>
